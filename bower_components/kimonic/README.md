f471041239a86f13d0e62777add6d5b30cad240a
Harris Baptiste <hbaptiste@opensense.fr>
2019-04-05 18:10:39 +0200

    fixing opennote

--- bower_components/kimonic/README.md
+++
@@ -1,4 +0,0 @@
-kimonic
-=======
-
-js framework 
--- bower_components/kimonic/formfields/image/libs/mini-upload-form/assets/main.js
+++
@@ -1,3 +0,0 @@
-var muploadpath = "kimo/formfields/image/libs/mini-upload-form/assets/";
- define("jquery.ui.widget",[muploadpath+"js/jquery.ui.widget"],function(){});
-define([muploadpath+"js/jquery.1.9.1.min","jquery.ui.widget",muploadpath+"js/jquery.fileupload", muploadpath+"js/jquery.iframe-transport",muploadpath+"js/jquery.knob"],function(){});
